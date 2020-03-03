da66cb4379a1e23ff3c0d110ffacd7bce2ed89d5
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-03-02 23:40:47 -0800

    2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
-import {createStore} from 'redux';
+import {bindActionCreators, createStore} from 'redux';
 import reducer from "./reducer";
 import {dec, inc, rnd} from "./actions";
 
 const store = createStore(reducer);
+const {dispatch} = store;
+
+// const bindActionCreators = (action, dispatch) => (...args) => {
+//     dispatch(creator(...args));
+// };
+
+const incDispatch = () => dispatch(inc());
+const decDispatch = () => dispatch(dec());
+const rndDispatch = (payload) => dispatch(rnd(payload));
 
 document
     .getElementById('dec')
-    .addEventListener('click', () => {
-        store.dispatch(dec());
-    });
+    .addEventListener('click', decDispatch);
 
 document
     .getElementById('inc')
-    .addEventListener('click', () => {
-        store.dispatch(inc());
-    });
+    .addEventListener('click', incDispatch);
 
 document
     .getElementById('rnd')
     .addEventListener('click', () => {
         const payload = Math.floor(Math.random() * 10);
-        store.dispatch(rnd(payload));
+        rndDispatch(payload)
     });
 
 const update = () => {
