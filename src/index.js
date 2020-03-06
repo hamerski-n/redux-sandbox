ed8e108b44fbf118bd0f10915b73c6c0a5a84764
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-03-05 16:53:35 -0800

    connect() refactor. When second argument of connect is an object

--- src/index.js
+++ src/index.js
@@ -4,20 +4,11 @@ import {bindActionCreators, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from "./reducer";
 import App from './components/app';
-//import * as actions from "./actions";
 
 const store = createStore(reducer);
-//const {dispatch} = store;
-//const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-//const update = () => {
     ReactDOM.render(
     <Provider store={store}>
         <App/>
     </Provider>,
     document.getElementById('root'));
-//};
-
-//update();
-//store.subscribe(update);
-
